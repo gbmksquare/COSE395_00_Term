7af2e841e8fba873947f6f2a3d8d15459fa8572b
BumMo Koo <ksquareatm@gmail.com>
2018-06-14 00:44:25 +0900

    Added IR and LCD code.

--- arduino.ino
+++ arduino.ino
@@ -1,3 +1,6 @@
+#include <IRremote.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 #define True 1
 #define False 0
@@ -6,10 +9,19 @@
 #define Active True
 #define Inactive False
 
+// ---------- Declaration ----------
 int N;
 
+// Pin
 int cdsPins[] = {A0, A1, A2, A3, A4};
 int ledPins[] = {13, 12, 11, 10, 9};
+int IR_pin = 8;
+
+// LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// IR
+IRrecv irrecv(IR_pin);
 
 int targetStatus[] = {Inactive, Inactive, Inactive, Inactive, Inactive};
 int targetActivatedTime[] = {0, 0, 0, 0, 0};
@@ -21,6 +33,10 @@ int brightnessThreshold = 18;
 
 int score = 0;
 
+void configure();
+void configurePinMode();
+void configureIr();
+void configureLcd();
 void startGame();
 void endGame();
 void addRandomTarget();
@@ -31,30 +47,21 @@ void turnOnLed(int pin);
 void turnOffLed(int pin);
 int getBrightness(int pin);
 
-// Main
+// ---------- Main ----------
 void setup() {
-	Serial.begin(9600);
-	randomSeed(analogRead(0));
-
-	N = sizeof(cdsPins) / sizeof(cdsPins[0]);
-
-	// Set pin mode
-	for(int i = 0; i < N; i++) {
-		int currentPin = ledPins[i];
-		pinMode(currentPin, OUTPUT);
-	}
-	
-	for(int i = 0; i < N; i++) {
-		int currentPin = cdsPins[i];
-		pinMode(currentPin, INPUT);
-	}
-
+	configure();
+	configurePinMode();
+	configureIr();
+	configrueLcd();
 	startGame();
 }
 
 void loop() {
 	int currentTime = millis();
 
+	getIRSignal();
+
+	
 	// Detect if target is hit
 	for (int i=0; i<N; i++){
 		if (targetStatus[i] == Active) {
@@ -78,6 +85,37 @@ void loop() {
 	if (targetCount < maximumConcurrentTarget) {
 		addRandomTarget();
 	}
+	
+}
+
+// ---------- Setup ----------
+void configure() {
+	Serial.begin(9600);
+	randomSeed(analogRead(0));
+	N = sizeof(cdsPins) / sizeof(cdsPins[0]);
+}
+
+void configurePinMode() {
+	for(int i = 0; i < N; i++) {
+		int currentPin = ledPins[i];
+		pinMode(currentPin, OUTPUT);
+	}
+	
+	for(int i = 0; i < N; i++) {
+		int currentPin = cdsPins[i];
+		pinMode(currentPin, INPUT);
+	}
+
+	pinMode(IR_pin, INPUT);
+}
+
+void configureIr() {
+	irrecv.enableIRIn();
+}
+
+void configrueLcd() {
+	lcd.init();
+	lcd.backlight();
 }
 
 // ---------- Game ----------
@@ -133,3 +171,34 @@ int getBrightness(int pin) {
 	return analogRead(pin);
 }
 
+// IR
+int getIRSignal() {
+	// U 16575
+	// L 49215
+	// R 41055
+	// D 24735
+	// Center 8415
+	// B 255
+	// V 32895
+	// Press 65535
+
+	decode_results results;
+	if (irrecv.decode(&results)) {
+		Serial.println(results.value & 0xFFFF);
+    	irrecv.resume();
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+// LCD
+void printLcd(int line, char *string) {
+	lcd.setCursor(0, line);
+	lcd.print(string);
+}
+
+void clearLcd() {
+	lcd.clear();
+}
